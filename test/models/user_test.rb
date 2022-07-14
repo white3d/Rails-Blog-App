6b5e75ce3b033a86307d2e20288e87b947d1773b
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-07-15 01:14:39 +0300

    Add Spec Files

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,22 +1,7 @@
 require 'test_helper'
 
-RSpec.describe User, type: :model do
-  it 'User should not be valid without valid attributes' do
-    expect(User.new).to_not be_valid
-  end
-
-  it 'User should not be valid without a name' do
-    person = User.new(name: nil)
-    expect(person).to_not be_valid
-  end
-
-  it 'post counter should not be valid without integer' do
-    person = User.new(post_counter: nil)
-    expect(person).to_not be_valid
-  end
-
-  it 'Most recent user returns only three posts' do
-    person = User.most_recent_posts.length
-    expect(person).to be <= 3
-  end
+class UserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
 end
