3001ab487888044dcfcbc2db81f09fac74f8578a
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-07-19 22:07:36 +0300

    Edit Linters

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show new create] do
-      resources :comments, only: [:create]
+      resources :comments, only: [:create, :new]
     end
   end
 
