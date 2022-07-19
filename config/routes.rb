48ff003489c4218d3b7581d1706394582c76d96a
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-07-19 22:11:14 +0300

    Edit Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/', to: 'users#index'
+  root 'users#index'
+
+  get 'posts/new', to: 'posts#new'
+  get 'comments/new', to: 'comments#new'
+
+  post 'posts/', to: 'posts#create'
+  post 'comments/', to: 'comments#create'
+
+  post '/users/:user_id/posts/:id/likes', to: 'likes#create'
 
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show new create] do
       resources :comments, only: [:create, :new]
     end
   end
-
-  put 'user/:user_id/post/:id/like', to: 'posts#like', as: 'like'
+  
 end
