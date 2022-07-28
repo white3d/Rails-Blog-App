fe5136c22042ad216be2b519493d6efa11b93c91
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-07-28 16:19:49 +0300

    Edit Posts Controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 class PostsController < ApplicationController
   def index
-    @user = User.find_by(id: params[:user_id])
-    @posts = Post.all.where(author_id: params[:user_id])
+    @user = User.find(params[:user_id])
+    @posts = @user.posts.includes(:comments)
   end
 
   def show
-    @user = User.find_by(id: params[:user_id])
-    @post = Post.find_by(id: params[:id], author_id: params[:user_id])
-    @comments = Comment.all.where(post_id: params[:id])
+    @post = Post.find(params[:id])
+    @user = @post.author
+    @comments = @post.comments
   end
 
   def new
@@ -15,29 +17,21 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(post_params)
-    @post.author_id = params[:user_id]
-    if @post.save
-      @post.update_counter
-      flash[:success] = 'Object successfully created'
-      redirect_to user_post_path(id: @post.id, user_id: @post.author_id)
-    else
-      flash[:error] = 'Something went wrong'
-      render 'new'
+    @new_post = current_user.posts.new(post_params)
+    respond_to do |format|
+      format.html do
+        if @new_post.save
+          redirect_to "/users/#{@new_post.author.id}/posts/", flash: { alert: 'Your post is saved' }
+        else
+          redirect_to "/users/#{@new_post.author.id}/posts/new", flash: { alert: 'Could not save post' }
+        end
+      end
     end
-  end
-
-  def like
-    @user = User.find_by(id: params[:user_id])
-    @post = Post.find_by(id: params[:id], author_id: params[:user_id])
-    @like = Like.create(post_id: @post.id, Author_id: current_user.id)
-    @like.update_likes_counter
-    redirect_to user_post_path(@user, @post)
   end
 
   private
 
   def post_params
-    params.require(:post).permit(:Title, :Text)
+    params.require(:post).permit(:title, :text)
   end
 end
