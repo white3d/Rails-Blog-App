b42cf40fec11448d4de9f6f71dd78a808bb838b1
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-07-28 19:09:53 +0300

    Edit User Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,15 @@
 class UsersController < ApplicationController
   def index
+    redirect_to new_user_session_path if current_user.nil?
     @users = User.all
+    @current_user = current_user
   end
 
   def show
-    @user = current_user
+    if params[:id] == 'sign_out'
+      redirect_to new_user_session_path
+    else
+      @user = current_user
+    end
   end
 end
